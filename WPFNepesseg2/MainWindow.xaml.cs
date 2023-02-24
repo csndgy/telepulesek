1043555f11ba65596fb06c57a1c79c294d0dffe2
csndgy <73069087+csndgy@users.noreply.github.com>
2023-02-24 16:17:58 +0100

    Add files via upload

---
+++ WPFNepesseg2/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPFNepesseg2.Model;
+
+namespace WPFNepesseg2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        List<Telepules> telepulesek;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            telepulesek = new List<Telepules>();
+
+            StreamReader sr = new StreamReader("Adatok\\kozerdeku_lakossag_2022.csv");
+            sr.ReadLine();
+            while (!sr.EndOfStream)
+            {
+                string CSVsor = sr.ReadLine();
+                string[] mezok = CSVsor.Split(";");
+                Telepules ujTelepules = new Telepules(mezok[2],
+                                                            mezok[3],
+                                                            mezok[4],
+                                                            int.Parse(mezok[5].Replace(" ", "")),
+                                                            int.Parse(mezok[6].Replace(" ", "")));
+
+                telepulesek.Add(ujTelepules);
+            }
+            
+            sr.Close();
+            dgTelepulesek.ItemsSource = telepulesek;
+
+        }
+
+        private void cnMegyek_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WPFNepesseg2/Model/Telepules.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFNepesseg2.Model
+{
+    public class Telepules
+    {
+        String megye;
+        String telepulesNev;
+        String telepulesTipus;
+        int ferfiLakosokSzama;
+        int noiLakosokSzama;
+
+        public Telepules(string megye, string telepulesNev, string telepulesTipus, int ferfiLakosokSzama, int noiLakosokSzama)
+        {
+            this.Megye = megye;
+            this.TelepulesNev = telepulesNev;
+            this.TelepulesTipus = telepulesTipus;
+            this.FerfiLakosokSzama = ferfiLakosokSzama;
+            this.NoiLakosokSzama = noiLakosokSzama;
+        }
+
+        public string Megye { get => megye; set => megye = value; }
+        public string TelepulesNev { get => telepulesNev; set => telepulesNev = value; }
+        public string TelepulesTipus { get => telepulesTipus; set => telepulesTipus = value; }
+        public int FerfiLakosokSzama { get => ferfiLakosokSzama; set => ferfiLakosokSzama = value; }
+        public int NoiLakosokSzama { get => noiLakosokSzama; set => noiLakosokSzama = value; }
+
+        public int LakosokSzamaEgyutt { get => this.ferfiLakosokSzama + this.noiLakosokSzama; }
+    }
+}
